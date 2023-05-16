6a7c8ab0f9f8f67a6b2e76fca1021b92ea24d4dc
Uttam Galoriya <19ce037@gardividyapith.ac.in>
2023-05-16 17:39:07 +0530

    delete unnecessary folder

--- src/app/addpipe/tabx.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-@Pipe({ name: 'tabx' })
-export class TabxPipe implements PipeTransform {
-
-    transform(value: any, ...args: any[]) {
-        return value.length
-    }
-
-}
